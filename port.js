70f38f7c618d9a2031ca048981162413232a0d42
Devanshi Srivastava <161362258+Devanshi12srivastava@users.noreply.github.com>
2024-12-01 22:48:43 +0530

    Add files via upload
    My portfolio

---
+++ port.js
@@ -0,0 +1,12 @@
+/*let btn=document.querySelector(".button");
+
+    btn.onclick=function(){
+        alert("submitted");
+    
+    };*/
+
+    let inp=document.querySelector("button");
+    inp.addEventListener("click", function(){
+        console.log("typed success");
+    });
+    console.log("hugt");
