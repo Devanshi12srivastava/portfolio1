e17f6c76a988f052cfc17a500a111a43b6895890
Devanshi Srivastava <161362258+Devanshi12srivastava@users.noreply.github.com>
2025-04-25 23:35:21 +0530

    Update port.js

--- port.js
+++ port.js
@@ -1,12 +1,13 @@
-/*let btn=document.querySelector(".button");
+const scriptURL = "https://script.google.com/macros/s/AKfycbzOTM-AB2jQ2s2Ls9vA9QOzbfrTyfsA4-8PDX1-6n4tQMGmFFwHkSf4WtEI2qzLmA5U9g/exec";
 
-    btn.onclick=function(){
-        alert("submitted");
-    
-    };*/
+const form = document.forms['contact-form']
 
-    let inp=document.querySelector("button");
-    inp.addEventListener("click", function(){
-        console.log("typed success");
-    });
-    console.log("hugt");
+form.addEventListener('submit', e => {
+  
+  e.preventDefault()
+  
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! Form is submitted" ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+});
